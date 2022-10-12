fe99b3da308c782b408a3e9a99c638db534e8620
LeAnn Ward <66979049+leanngward@users.noreply.github.com>
2022-10-12 13:38:51 -0400

    Update and rename make_submission_list.py to makelist_of_control_files.py
    Made more general to to the name of control files for use with gnu parallel. Also, removed extra space character that codeml can't read.

--- RunEvolutionModels/Submitting Jobs/makelist_of_control_files.py
+++ RunEvolutionModels/Submitting Jobs/makelist_of_control_files.py
@@ -2,13 +2,13 @@ import os
 import sys
 import re
 
-#Provide directory that contains the .sh files
+#Provide directory that contains the files of interest
 dirName = sys.argv[1]
 
-#Provide the string pattern for the .sh files that you want to record
+#Provide the string pattern for the file names that you want to record
 stringFlag = sys.argv[2]
 
-#Provide the output file name for the list of .sh files
+#Provide the output file name for the list of files
 outName = sys.argv[3]
 outFile = open(outName,'w')
 
@@ -16,6 +16,7 @@ for filename in os.listdir(dirName):
 	f = os.path.join(dirName,filename)
 	if os.path.isfile(f):
 		if re.search(stringFlag,f):
+			f = f.strip()
 			outFile.write(f+"\n")
 
 
